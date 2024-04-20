1127e33bef4bcf7ad25a5cc3c7e11de039781d80
skovtrolden <asger1231@gmail.com>
2024-04-20 22:07:31 +0200

    Input changes

--- Assets/Scripts/TorchBehaviour.cs
+++ Assets/Scripts/TorchBehaviour.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
 
 public class TorchBehaviour : MonoBehaviour
 {
     private new Light2D light;
+    bool on = true;
     // Start is called before the first frame update
     void Awake()
     {
@@ -19,7 +22,33 @@ public class TorchBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Flicker();
+        if (on)
+            Flicker();
+    }
+
+    public void OnConceal(InputAction.CallbackContext context){
+        if (context.started)
+        {
+            Conceal();
+        }
+        if (context.canceled)
+        {
+            Reveal();
+        }
+    }
+
+    public void Reveal()
+    {
+        on = true;
+        light.intensity = 1;
+        light.pointLightOuterRadius = 1;
+    }
+
+    public void Conceal()
+    {
+        on = false;
+        light.intensity = 0;
+        light.pointLightOuterRadius = 0;
     }
 
     private float flickerSpeed = 0.1f;
@@ -33,7 +62,7 @@ public class TorchBehaviour : MonoBehaviour
         {
             float strength = Random.Range(0.8f, 1.2f);
             light.intensity = strength;
-            light.pointLightOuterRadius = strength/2 + 4.5f;
+            light.pointLightOuterRadius = strength/2 + 2.5f;
 
             flickerTimer = 0f;
         }
