cbbd6855f84152d39da0e9dd07ebcf4af70d5c35
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2024-04-21 11:20:37 +0200

    background torches

---
+++ Assets/Scripts/BackgroundTorch.cs
@@ -0,0 +1,35 @@
+using TarodevController;
+using UnityEngine;
+
+public class BackgroundTorch : MonoBehaviour
+{
+	public GameObject flame;
+	public bool startOn = false;
+
+	private void Start()
+	{
+		if (startOn)
+			TurnOn();
+	}
+
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			if (other.isTrigger)
+				return;
+
+			var player = other.GetComponent<PlayerController>();
+			if (player == null)
+				return;
+			//if (player.FrameInput.LightOn)
+			//	TurnOn();
+			print("player in range");
+		}
+    }
+
+	private void TurnOn()
+	{
+		flame.SetActive(true);
+	}
+}
